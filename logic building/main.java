8b01b2da5b2af2d0abf1e560f04d8a1ce92cc87c
Yuvraj Thakur <yuvraj5sehore@gmail.com>
2026-08-25 15:29:06 +0530

    logic program

---
+++ logic building/main.java
@@ -0,0 +1,12 @@
+// going to write my first java program for logic building 
+
+public class main{
+    public static void main(String[] args) {
+        int x = 2;
+        int y = 2;
+
+        int sum = x+y;
+
+        System.out.println(sum);
+    }
+}
