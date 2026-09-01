def0fb85468edd0fd95426deebb38647727894e7
Yuvraj Thakur <yuvraj5sehore@gmail.com>
2026-09-01 19:34:09 +0530

    MaxSubarraysum optimized

--- Arrays/maxSubarraySum.java
+++ Arrays/maxSubarraySum.java
@@ -1,9 +1,10 @@
 // How to print Maximum Subarrays sum of an array 
-// Brute force approach
+// Brute force approach  //prefix sum approach  // kadense algo
 
 public class maxSubarraySum{  
     public static void maxSubarraySum(int numbers[]){
-        int cs = 0;
+        // bruteforce approach 
+      /*  int cs = 0;
         int ms = Integer.MIN_VALUE;
         for(int i = 0; i<numbers.length; i++){
             int start = i;
@@ -21,7 +22,30 @@ public class maxSubarraySum{
             }
            
         }
-         System.out.println("max sum ="+ms);
+         System.out.println("max sum ="+ms); */
+         // prefix sum approach
+         int cs = 0;
+         int ms = Integer.MIN_VALUE;
+
+         int prefix[] = new int [numbers.length];
+
+         prefix[0] = numbers[0];
+
+         for(int i = 1;i<prefix.length;i++){
+            prefix[i] = prefix[i-1] + numbers[i];
+         }
+         for(int i = 0;i<numbers.length;i++){
+            int start = i;
+            for(int j = 0;j<numbers.length;j++){
+             int end = j;
+             cs = start == 0 ? prefix[end] : prefix[end] - prefix[start - 1];
+             if(ms<cs){
+                ms = cs;
+             }
+            }
+         }
+
+        System.out.println("max sum = "+ms); 
     }
     public static void main(String args[]){  
        int numbers[] = {1,-2,6,-1,3};          
