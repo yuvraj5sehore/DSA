a22733ada95985bf86e3838e991d4e4a55389cb9
Yuvraj Thakur <yuvraj5sehore@gmail.com>
2026-09-09 10:16:50 +0530

    valid circle completed

---
+++ logic building/validCircle.java
@@ -0,0 +1,13 @@
+
+//valid circle check 
+class validCircle {
+    public static void main(String[] args) {
+        int radius = 12;
+
+        if(radius<=0){
+            System.out.println("Invalid circle");
+        }else{
+            System.out.println("Valid circle");
+        }
+    }
+}
