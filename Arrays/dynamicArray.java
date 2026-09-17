9a1ed97f37a4663d96f3061bfdad2cfd77345293
Yuvraj Thakur <yuvraj5sehore@gmail.com>
2026-09-17 21:27:39 +0530

    dynamic Array

---
+++ Arrays/dynamicArray.java
@@ -0,0 +1,25 @@
+
+//how to make an dynamic array in java 
+import java.util.*;
+
+class dynamicArray {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(10);
+        arr.add(20);
+        arr.add(30);
+        arr.add(40);
+
+        System.out.println("Array Elements are:");
+        for (int i = 0; i < arr.size(); i++) {
+            System.out.print(arr.get(i) + " ");
+        }
+        // removing last element
+        arr.remove(arr.size() - 1);
+        System.out.println("\nAfter removing last element");
+        for (int i = 0; i < arr.size(); i++) {
+            System.out.print(arr.get(i) + " ");
+        }
+    }
+}
